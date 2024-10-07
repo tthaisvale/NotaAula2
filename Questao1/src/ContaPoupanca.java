bc5c87e38244f431513bd1fc16a2dcf511ed94af
Thais Vale <126836029+tthaisvale@users.noreply.github.com>
2024-10-07 20:17:34 -0300

    ContaPoupanca.java

--- Questao1/src/ContaPoupanca.java
+++ Questao1/src/ContaPoupanca.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+public class ContaPoupanca extends ContaBancaria {
+    private double selic;
 
-public class ContaPoupanca {
+    // Construtor
+    public ContaPoupanca(String titular, double selic) {
+        super(titular);  // Chama o construtor da classe base (ContaBancaria)
+        this.selic = selic;
+    }
+
+    // Método para calcular o rendimento com base na taxa Selic
+    public void calcularRendimento() {
+        double rendimento;
+        if (selic > 8.5) {
+            rendimento = 0.005 * saldo;
+        } else {
+            rendimento = 0.007 * selic * saldo;
+        }
+        saldo += rendimento;
+        System.out.println("Rendimento calculado: R$ " + rendimento);
+        System.out.println("Novo saldo: R$ " + saldo);
+    }
+
+    // Exibir dados da conta poupança
+    @Override
+    public void exibirDados() {
+        super.exibirDados();
+    }
 }
