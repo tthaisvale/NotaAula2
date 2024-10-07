ce89e67f008e82f982c6417d3059ddf42642804d
Thais Vale <126836029+tthaisvale@users.noreply.github.com>
2024-10-07 20:16:58 -0300

    ContaCorrente.java

--- Questao1/src/ContaCorrente.java
+++ Questao1/src/ContaCorrente.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+public class ContaCorrente extends ContaBancaria {
+    private double chequeEspecial = 1000.0;
 
-public class ContaCorrente {
+    // Construtor
+    public ContaCorrente(String titular) {
+        super(titular);  // Chama o construtor da classe base (ContaBancaria)
+    }
+
+    // Sobrescreve o método sacar para incluir o cheque especial
+    @Override
+    public void sacar(double valor) {
+        if (valor > 0 && (saldo + chequeEspecial) >= valor) {
+            saldo -= valor;
+            System.out.println("Saque de R$ " + valor + " realizado com sucesso (com cheque especial, se necessário).");
+        } else {
+            System.out.println("Saldo insuficiente, mesmo com cheque especial.");
+        }
+    }
+
+    // Exibir dados da conta corrente (inclui cheque especial)
+    @Override
+    public void exibirDados() {
+        super.exibirDados();
+        System.out.println("Cheque especial disponível: R$ " + chequeEspecial);
+    }
 }
