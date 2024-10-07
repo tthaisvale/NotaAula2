88933222ce2c368e5fcb92584e2293c0ec429cd6
Thais Vale <126836029+tthaisvale@users.noreply.github.com>
2024-10-07 20:16:24 -0300

    ContaBancaria.java

--- Questao1/src/ContaBancaria.java
+++ Questao1/src/ContaBancaria.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
-
 public class ContaBancaria {
+    protected String titular;
+    protected double saldo;
+
+    // Construtor
+    public ContaBancaria(String titular) {
+        this.titular = titular;
+        this.saldo = 0.0;
+    }
+
+    // Método para depositar dinheiro na conta
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+            System.out.println("Depósito de R$ " + valor + " realizado com sucesso.");
+        } else {
+            System.out.println("Valor inválido para depósito.");
+        }
+    }
+
+    // Método para sacar dinheiro da conta (será sobrescrito na ContaCorrente)
+    public void sacar(double valor) {
+        if (valor > 0 && saldo >= valor) {
+            saldo -= valor;
+            System.out.println("Saque de R$ " + valor + " realizado com sucesso.");
+        } else {
+            System.out.println("Saldo insuficiente ou valor inválido.");
+        }
+    }
+
+    // Método para exibir os dados da conta
+    public void exibirDados() {
+        System.out.println("Titular: " + titular);
+        System.out.println("Saldo: R$ " + saldo);
+    }
 }
